1bffc2b06a1e7f346a6be30af8d9d75b8eab1388
NikSavT <hickhickert@gmail.com>
2022-11-28 14:14:27 +0300

    feat: add task 61

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const whoIsThisHouseToStarks = (houseName) => {
 };
 // END */
 
-const whoIsThisHouseToStarks = (family) => {
+/* const whoIsThisHouseToStarks = (family) => {
   if (family === 'Karstark' || family === 'Tally') {
     return 'friend';
   }
@@ -41,4 +41,63 @@ const whoIsThisHouseToStarks = (family) => {
     return 'neutral';
   }
 };
-console.log(whoIsThisHouseToStarks('NickSova'));//neutral
+console.log(whoIsThisHouseToStarks('NickSova'));//neutral */
+
+/* Задание 61
+
+Реализуйте функцию convertText(), которая принимает на вход строку и, если первая буква не заглавная, возвращает перевернутый вариант исходной строки. Если первая буква заглавная, то строка возвращается без изменений. Если на вход передана пустая строка, функция должна вернуть пустую строку.
+
+Примеры вызова:
+
+convertText('Hello'); // 'Hello'
+convertText('hello'); // 'olleh'
+
+// Не забудьте учесть пустую строку
+convertText(''); // ''
+Перевернуть строчку можно используя функцию 
+const reverse = (str) => str.split('').reverse().join('');
+ В качестве аргумента в неё нужно передать строку, которую мы хотим перевернуть:
+
+const result = reverse('Hello!');
+console.log(result); // => '!olleH'
+Есть разные подходы к решению этой задачи. Возможно, вам пригодится метод toUpperCase() и возможность получения символа из строки (например, str[0]).
+
+Попробуйте написать два варианта функции: с обычным if-else, и с тернарным оператором.
+
+
+Решение*/
+/* Решение учителя:
+const reverse = (str) => str.split('').reverse().join('');
+const convertText = (text) => {
+  if (text === '') {
+    return '';
+  }
+
+  const reversable = text[0] !== text[0].toUpperCase();
+  return reversable ? reverse(text) : text;
+}; */
+
+
+const reverse = (str) => str.split('').reverse().join('');// функция дана из условия
+/* const convertText = (text) => {
+  if (text === '') {
+  return '';
+}
+  if (text[0] === text[0].toUpperCase()) {
+  return text;
+}
+  return reverse(text);
+}; */
+// Решение через тернальный оператор (применяется дважды, что не рекомендуется)
+// const convertText = (text) => (text === '' ? '' : (text[0] === text[0].toUpperCase() ? text : reverse(text)));
+
+// Решение через один тернальный оператор:
+const convertText = (text) => {
+  if (text === '') {
+  return '';
+  }
+  const stringStart = text[0] === text[0].toUpperCase;
+  return stringStart ? text : reverse(text);
+};
+
+console.log(convertText('mike Vasovski'));//iksvosaV ekim
